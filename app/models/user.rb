b2f8c3eafed8d98a3a43ff49eb68f8bfd8ae3d6d
chungyee86 <buer5011@yahoo.com>
2017-04-06 18:18:09 +0800

    google API is applied

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   include Clearance::User
+
+  # geocoded_by :address
+  # after_validation :geocode, if: :address_changed?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406062110) do
+ActiveRecord::Schema.define(version: 20170406074931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20170406062110) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
+    t.float    "latitude"
+    t.float    "longtitude"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
