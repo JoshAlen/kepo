9e7cf13347bfe3391bc79500f399001a3ee6e269
chungyee86 <buer5011@yahoo.com>
2017-04-07 10:36:27 +0800

    able to retrieve latitude and longtitude

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments
 	mount_uploaders :images, ImagesUploader
+
+	reverse_geocoded_by :latitude, :longitude
+	after_validation :reverse_geocode  # auto-fetch address
 end
