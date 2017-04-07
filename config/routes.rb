d4a06e5fb2d707b898d55d74dc69072864940d29
Joshua Seitz <SeitzJoshua7@gmail.com>
2017-04-07 16:13:14 +0800

    add more stuff to fontend

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
-  root to: 'welcomes#index'
+  
+  root to: 'posts#index'
 
   resources :welcomes, only: [:index]
 
