af76ae522773443a444088fc1a75b36b78823757
shock1234 <perfect.1988@hotmail.com>
2017-04-11 17:57:09 +0800

    first commit

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,7 +1,32 @@
 $(document).on("turbolinks:load",function(){
 	$.each($(".timer-container"), function(index,domElement){
 		$(domElement).countdown({until: new Date($(domElement).data("date"))});
+		if(new Date($.now()) >= new Date($(domElement).data("date"))){
+			$(".timer-container").remove()
+		}
 	});
 	
 });
+
+
+
+
+// $(document).on("turbolinks:load",
+	
+// );
+
+
+// function timer(){
+// 	$.each($(".timer-container"), function(index,domElement){
+// 		$(domElement).countdown({until: new Date($(domElement).data("date"))});
+// 	});
+// }
+
+
+
+// if(new Date($.now()) >= new Date($(domElement).data("date")))
+// {
+// 	$(".timer-container").remove()
+// }
+
 
